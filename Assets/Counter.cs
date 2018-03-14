b7312be1d85c8e6d1dfc080b203b2286e4cf7ecf
MoongK <qorfyd01@gmail.com>
2018-03-14 16:59:04 +0900

    first

---
+++ Assets/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    public int count;
+
+    Text counter;
+
+    private void Awake()
+    {
+        count = 15;
+    }
+    void Start () {
+        counter = GetComponent<Text>();
+        StartCoroutine(StartCounter());
+	}
+
+    IEnumerator StartCounter()
+    {
+        while (count >= 0)
+        {
+            counter.text = count.ToString();
+            count--;    
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    private void Update()
+    {
+        if (count < 10)
+            counter.color = Color.red;
+    }
+
+}
---
+++ Assets/Damaged.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damaged : MonoBehaviour {
+
+    Image im;
+    float hp;
+
+	void Start () {
+        im = GetComponent<Image>();
+        hp = im.rectTransform.localScale.x;
+	}
+
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            im.rectTransform.localScale += Vector3.left * 0.1f;
+
+        if (im.rectTransform.localScale.x <= 0f)
+            im.rectTransform.localScale = new Vector3(0f, 1f, 1f);
+
+        //if (im.rectTransform.localScale.x >= 1f)
+        //    im.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+
+    }
+}
